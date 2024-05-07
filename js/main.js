fa04dc7dcb2ea433b6e2d24274373a91a1cb1635
vladyslav <vladnazarchuk406@gmail.com>
2024-05-07 11:57:55 +0200

    comment

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+   $('.bike-slider').slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000 //ms,
+   }); 
+   $('.slider__items').slick({
+      arrows: false,
+      dots: true,
+      fade: true,
+      autoplay: true,
+      autoplaySpeed: 2750 //ms
+   });
+
+});
